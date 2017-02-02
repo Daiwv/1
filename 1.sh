43f3002c0e37b79d19b566e62c1edbf4704862af
Daiwv <daiw@mail.ru>
2017-02-02 15:15:54 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-echo "ПОГНАЛИ !"
 while true
 do
-echo "МАЙН !"
-sudo cpulimit -e minergate-cli -v -l 120&sleep 10&sudo minergate-cli -user boss.velial@bk.ru -xmr&sleep 120&&sudo killall -9 minergate-cli&&sudo killall -9 cpulimit
-echo "СТОП !"
+sudo cpulimit -e minergate-cli -v -l 50&sleep 10&sudo minergate-cli -user boss.velial@bk.ru -xmr&sleep 120&&sudo killall -9 minergate-cli&&sudo killall -9 cpulimit
 sleep 30
 done
 echo
