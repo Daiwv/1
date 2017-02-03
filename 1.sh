ebec61c94418c06cac2a4e5ee66e6ed09d369c04
Daiwv <daiw@mail.ru>
2017-02-03 13:43:35 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 while true
 do
-sudo cpulimit -e minergate-cli -v -l 100&sleep 10&sudo minergate-cli -user boss.velial@bk.ru -xmr&sleep 60&&sudo killall -9 minergate-cli&&sudo killall -9 cpulimit&sleep 30&sudo cpulimit -e minergate-cli -v -l 120&sleep 10&sudo minergate-cli -user boss.velial@bk.ru -xmr&sleep 120&&sudo killall -9 minergate-cli&&sudo killall -9 cpulimit
-sleep 60
+echo "-=START=-"
+sleep 5
+sudo cpulimit -e minergate-cli -v -l 100&echo "-=CONTROL1=-"&sleep 10
+sudo minergate-cli -user boss.velial@bk.ru -xmr&echo "-=START1=-"&sleep 60
+sudo killall -9 minergate-cli&sudo killall -9 cpulimit&echo "-=KILL1=-"&sleep 30
+sudo cpulimit -e minergate-cli -v -l 80&echo "-=CONTROL2=-"&sleep 10
+sudo minergate-cli -user boss.velial@bk.ru -xmr&echo "-=START2=-"&sleep 120
+sudo killall -9 minergate-cli&sudo killall -9 cpulimit&echo "-=KILL2=-"&sleep 60
+sudo cpulimit -e minergate-cli -v -l 120&echo "-=CONTROL3=-"&sleep 10
+sudo minergate-cli -user boss.velial@bk.ru -xmr&echo "-=START3=-"&sleep 180
+sudo killall -9 minergate-cli&sudo killall -9 cpulimit&echo "-=KILL3=-"&sleep 45
+sudo cpulimit -e minergate-cli -v -l 150&echo "-=CONTROL4=-"&sleep 10
+sudo minergate-cli -user boss.velial@bk.ru -xmr&echo "-=START4=-"&sleep 30
+sudo killall -9 minergate-cli&sudo killall -9 cpulimit&echo "-=KILL4=-"&sleep 60
+echo "-=END=-"
+sleep 5
 done
 echo
