8ae08436a426287dc5fbe2ba8f5236d52a6bb859
Daiwv <daiw@mail.ru>
2017-02-03 14:34:52 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 while true
 do
-sudo cpulimit -e minergate-cli -v -l 150&sleep 10&sudo minergate-cli -user boss.velial@bk.ru -xmr&sleep 200&&sudo killall -9 minergate-cli&&sudo killall -9 cpulimit
-sleep 30
+echo "-=START=-"
+sleep 5
+sudo cpulimit -e minergate-cli -v -l 150&echo "-=CONTROL=-"&sleep 10
+sudo minergate-cli -user boss.velial@bk.ru -xmr&echo "-=START=-"&sleep 150
+sudo killall -9 minergate-cli&sudo killall -9 cpulimit&echo "-=KILL=-"&sleep 45
+echo "-=END=-"
+sleep 5
 done
 echo
