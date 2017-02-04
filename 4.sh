35268fa0290f01f51eeb9c939ae9fb73286e1bea
Daiwv <daiw@mail.ru>
2017-02-04 16:00:52 +0700

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -4,7 +4,7 @@ do
 echo "-=START=-"
 sleep 5
 sudo cpulimit -e minergate-cli -v -l 300&echo "-=CONTROL=-"&sleep 10
-sudo minergate-cli -user boss.velial@bk.ru -xmr -qcn&echo "-=START=-"&sleep 300
+sudo minergate-cli -user boss.velial@bk.ru -xmr&echo "-=START=-"&sleep 300
 sudo killall -9 minergate-cli&sudo killall -9 cpulimit&echo "-=KILL=-"&sleep 60
 echo "-=END=-"
 sleep 5
